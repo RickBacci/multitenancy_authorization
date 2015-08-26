afcb289ff496df0b1a8ae951e12ceba82b98f591
Rick Bacci <rickbacci@gmail.com>
2015-08-26 10:17:49 -0600

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,26 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  add_flash_types :success, :info, :warning, :danger
+
+  helper_method :current_user
+
+  before_action :authorize!
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_permission
+    @current_permission ||= PermissionService.new(current_user)
+  end
+
+  def authorize!
+    unless current_permission.allow?(params[:controller], params[:action])
+      redirect_to root_url, danger: "You don't know me."
+    end
+  end
+
+end
---
+++ app/services/permission_service.rb
@@ -0,0 +1,18 @@
+class PermissionService
+  attr_reader :user
+
+  def initialize(user)
+    @user = user || User.new
+  end
+
+  def allow?(controller, action)
+    if user.registered_user?
+      return true if controller == 'stores' && action.in?(%w(index show))
+      return true if controller == 'sessions' && action.in?(%w(new create destroy))
+    else
+      return true if controller == 'stores' && action == 'index'
+      return true if controller == 'sessions' && action.in?(%w(new create))
+    end
+  end
+end
+
